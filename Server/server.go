d562a030a72037a3a9ffea44747f6c26af44d701
Surfix <Surfix@DESKTOP-UIPFAVG>
2021-01-09 09:06:32 +0100

    Advanced server

--- Server/server.go
+++ Server/server.go
@@ -5,8 +5,15 @@ import (
 	"net/http"
 )
 
-const url = "http://localhost"
+const url = "http://localhost/"
 const port = ":8090"
+
+/*
+type PageVariables struct {
+	Date string
+	Time string
+}
+*/
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "hello\n")
@@ -15,8 +22,26 @@ func hello(w http.ResponseWriter, req *http.Request) {
 func startpage(w http.ResponseWriter, req *http.Request) {
 
 	var template string = "<h1>Startpage</h1><p>Welcome to your Golang Website</p>"
-
 	fmt.Fprintf(w, template)
+
+	/* More Adcanved with html/template package
+
+	now := time.Now()              // find the time right now
+	HomePageVars := PageVariables{ //store the date and time in a struct
+		Date: now.Format("02-01-2006"),
+		Time: now.Format("15:04:05"),
+	}
+
+	t, err := template.ParseFiles("startpage.html") //parse the html file homepage.html
+	if err != nil {                                 // if there is an error
+		log.Print("template parsing error: ", err) // log it
+	}
+	err = t.Execute(w, HomePageVars) //execute the template and pass it the HomePageVars struct to fill in the gaps
+	if err != nil {                  // if there is an error
+		log.Print("template executing error: ", err) //log it
+	}
+	*/
+
 }
 
 func main() {
